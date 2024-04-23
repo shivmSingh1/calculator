2d764fbab9005c00476a6f6b6afce7fd07535426
shivmSingh1 <shivams7380@gmai.com>
2024-04-24 03:15:20 +0530

    modify app.jsx added operations logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,24 @@ import "./App.css"
 
 function App() {
 
-  const [calValue, setCalValue] = useState("")
+  const [calValue, setCalValue] = useState("");
+  const operators = ['+', '-', '*', '/'];
   const onButtonClick = (data) => {
     if (data === "c") {
       setCalValue("")
     } else if (data === "=") {
       const result = eval(calValue);
       setCalValue(result)
+    } else if (operators.includes(data)) {
+      // Check if the last character in calValue is an operator
+      if (operators.includes(calValue[calValue.length - 1])) {
+        // If so, replace it with the new operator
+        const newCalValue = calValue.slice(0, -1) + data;
+        setCalValue(newCalValue);
+      } else {
+        // Otherwise, append the operator to calValue
+        setCalValue(calValue + data);
+      }
     } else {
       const newDisplayValue = calValue + data;
       setCalValue(newDisplayValue)
